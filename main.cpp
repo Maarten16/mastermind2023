68cceab2d313091e43465c828e8fd6b6e51d451a
maarten16 <maarten.dejong@student.hu.nl>
2023-02-07 18:21:20 +0100

    started function that handles and validates user input of colors using asci

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <limits>
+#include <vector>
 
 #ifdef _WIN32
 #include <Windows.h>
@@ -8,86 +9,123 @@
 #endif
 
 #define inputfield "\n\t: "
-#define endsection std::string(30, '=')
+#define endsection string(30, '=')
 
-using std::cout; using std::cin;
+
+using std::cout; using std::cin; using std::string;
 
 int int_input(int minimum, int maximum);
 char gamemode_input();
 
 int main() {
     //adjustable variables
-    int minimumcolors = 2;
-    int maximumcolors = 9;
-    int minimumlength = 2;
-    int maximumlength = 9;
-    int minimumguesses = 4;
-    int maximumguesses = 20;
+    const int minimumcolors = 2;
+    const int maximumcolors = 9;
+    const int minimumlength = 2;
+    const int maximumlength = 9;
+    const int minimumguesses = 4;
+    const int maximumguesses = 20;
+    string colornames[maximumcolors] = {"blue", "red", "green", "orange", "purple", "yellow", "brown", "pink", "grey"};
 
     //code
     cout << "Hello, welcome to mastermind!\n";
 
             //gamemode
     cout << "Which gamemode do you want to play?\n";
-    cout << "Type 'A' if you player to make and guess the secret code\n";
-    cout << "Type 'B' if you want player to make and computer to guess the secret code\n";
-    cout << "Type 'C' if you want computer to make and player to guess the secret code\n";
-    cout << "type 'D' if you want computer to make and guess the secret code" << inputfield;
+    cout << "Type 'A' if you player to make and guess the code\n";
+    cout << "Type 'B' if you want player to make and computer to guess the code\n";
+    cout << "Type 'C' if you want computer to make and player to guess the code\n";
+    cout << "type 'D' if you want computer to make and guess the code" << inputfield;
     char gamemode = gamemode_input();
     cout << "You chose gamemode " << gamemode << "\n";
     cout << endsection << '\n' << '\n';
 
             //colors
-    cout << "With how many colors do you want to play?\nEnter a number between " << minimumcolors << " and " << maximumcolors << inputfield;
+    cout << "With how many colors do you want to play?\n" << "Enter a number between " << minimumcolors << " and " << maximumcolors << inputfield;
     int colors_number = int_input(minimumcolors, maximumcolors);
-    cout << "The number of colours you play with is: " << colors_number << "\n";
+    cout << "The number of colours you will play with is: " << colors_number << "\n";
     cout << endsection << '\n' << '\n';
 
             //length
-    cout << "How long do you want the secret code to be?\nEnter a number between " << minimumlength << " and " << maximumlength << inputfield;
-    int secret_length = int_input(minimumlength, maximumlength);
-    cout << "The secret code is " << secret_length << " pins long\n";
+    cout << "How long do you want the code to be?\n" << "Enter a number between " << minimumlength << " and " << maximumlength << inputfield;
+    int code_length = int_input(minimumlength, maximumlength);
+    cout << "The code is " << code_length << " pins long\n";
     cout << endsection << '\n' << '\n';
+
+            //guesses
+    cout << "How many guesses do you want to have?\n" << "Enter a number between " << minimumguesses << " and " << maximumguesses << inputfield;
+    int total_guesses = int_input(minimumguesses, maximumguesses);
+    cout << "You will have " << total_guesses << "guesses\n";
+    cout << endsection << '\n' << '\n';
+
+
+
+
+
+
     return 0;
 }
 
-    int int_input(int minimum, int maximum){
-        int num;
-        while (true) {
-            if (cin >> num && num >= minimum && num <= maximum) {
-                cin.clear();
-                cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-                return num;
-            } else {
-                cin.clear();
-                cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-                cout << "Invalid input! Please enter a number between " << minimum << " and " << maximum << inputfield;
-            }
-        }
-    }
-
-
-    char gamemode_input(){
-        char mode;
-        cin >> mode;
-        while(true){
+int int_input(int minimum, int maximum){
+    int num;
+    while (true) {
+        if (cin >> num && num >= minimum && num <= maximum) {
             cin.clear();
             cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            if (mode == 'a' || mode == 'A'){
-                return 'A';
+            return num;
+        } else {
+            cin.clear();
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            cout << "Invalid input! Please enter a number between " << minimum << " and " << maximum << inputfield;
+        }
+    }
+}
+
+
+char gamemode_input(){
+    char mode;
+    cin >> mode;
+    while(true){
+        cin.clear();
+        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        if (mode == 'a' || mode == 'A'){
+            return 'A';
+        }
+        else if (mode == 'b' || mode == 'B'){
+            return 'B';
+        }
+        else if (mode == 'c' || mode == 'C'){
+            return 'C';
+        }
+        else if (mode == 'd' || mode == 'D'){
+            return 'D';
+        }
+        else{
+            cout << "invalid input. Please give the letter 'A', 'B, 'C', or 'D" << inputfield;
+            cin >> mode;
+        }
+    }
+}
+
+string codeinput_user(int colors_num, int codelength, int &colors_name){
+    string code = "";
+    string color;
+    int ASCI_lower = 97;
+    int ASCI_upper = 65;
+
+
+    while(codelength >= 0){// will become validate_color
+        cin >> color;
+        if(color.size() == 1){
+            int asci = int(color[0]);
+            if (asci >= ASCI_upper && asci < ASCI_upper + colors_num){
+                code += color[0];
             }
-            else if (mode == 'b' || mode == 'B'){
-                return 'B';
-            }
-            else if (mode == 'c' || mode == 'C'){
-                return 'C';
-            }
-            else if (mode == 'd' || mode == 'D'){
-                return 'D';
-            }
-            else{
-                cout << "invalid input. Please give the letter 'A', 'B, 'C', or 'D" << inputfield;
-                cin >> mode;
+            else if(asci >= ASCI_lower && asci < ASCI_lower + colors_num){
+                asci -= 32;
+                code += char(asci);
             }
         }
+        else if()
+    }
 }
